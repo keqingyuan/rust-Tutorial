9b8dcdc01286061eb2c102d0d7f716659469fa6d
柯 清元 <qingyuan.ke@foxmail.com>
2022-06-17 00:31:30 +0800

    commit

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,12 +1,12 @@
 use std::env;
 use std::process;
 
-use minigrep::Config;
+mod core;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err|{
+    let config = core::Config::new(&args).unwrap_or_else(|err|{
         eprintln!("解析参数异常:{}",err);
         process::exit(1);
     });
@@ -15,7 +15,7 @@ fn main() {
     println!("检索文件:{}", config.filename);
 
     // let contents = fs::read_to_string(filename).expect("读文件错误");
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = core::run(config) {
         eprintln!("程序错误:{}",e);
         process::exit(1);
     }
